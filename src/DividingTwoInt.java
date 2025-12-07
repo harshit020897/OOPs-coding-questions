4b39aa04911c72033d1e5de35a2a9772cc7addb0
harshitkumar13-cloud <harshitkumar13@gmail.com>
2025-12-08 02:56:47 +0530

    This is my program for subtraction of two integers

---
+++ src/DividingTwoInt.java
@@ -0,0 +1,10 @@
+public class DividingTwoInt {
+    public static void main(String[] args)
+    {
+        int a = 40;
+        int b = 50;
+        int c = a-b;
+        System.out.println(c);
+    }
+
+}
