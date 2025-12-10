41318f7175e0798e459c87aada437d7346041c75
harshitkumar13-cloud <harshitkumar13@gmail.com>
2025-12-10 07:53:37 +0530

    This is my program for multiplication of two integers

---
+++ src/MultiplyTwoInt.java
@@ -0,0 +1,10 @@
+public class MultiplyTwoInt {
+    public static void main (String[] args)
+    {
+        int a = 50;
+        int b = 25;
+        int c = (a*b);
+        System.out.println(c);
+
+    }
+}
