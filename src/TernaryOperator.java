ca0e607ccd23a8b7278a84af3050fd13dab37423
harshitkumar13-cloud <harshitkumar13@gmail.com>
2025-12-29 00:12:00 +0530

    FizzBuzz Test Program

---
+++ src/TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int marks = 100;
+        String result = (marks >= 40) ? "Pass" : "Fail";
+        System.out.println(result);
+    }
+}
