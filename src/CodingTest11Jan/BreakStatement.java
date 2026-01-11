73d0b5fb84483ddea8d03c14bd216d0f53f92055
harshitkumar13-cloud <harshitkumar13@gmail.com>
2026-01-11 22:59:15 +0530

    BreakStatement

---
+++ src/CodingTest11Jan/BreakStatement.java
@@ -0,0 +1,14 @@
+package CodingTest11Jan;
+
+public class BreakStatement {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i);
+            if(i==5)
+            {
+                break;
+            }
+        }
+    }
+}
