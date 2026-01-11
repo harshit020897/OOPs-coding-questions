35bc29cbd8f533a410879d570f0e6dfb50c685fd
harshitkumar13-cloud <harshitkumar13@gmail.com>
2026-01-11 22:54:34 +0530

    PrintNum1to10

---
+++ src/CodingTest11Jan/PrintNum1to10.java
@@ -0,0 +1,14 @@
+package CodingTest11Jan;
+
+public class PrintNum1to10 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+                System.out.println(i);
+                i++;
+
+            }
+            while(i<=10);
+
+    }
+}
