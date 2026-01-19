8f70f14091dfb60779d57dc0c8d28060d17ba684
harshitkumar13-cloud <harshitkumar13@gmail.com>
2026-01-19 20:42:46 +0530

    Right triangle pattern left

---
+++ src/StringsAndArrays/RightTrianglePatternLeft.java
@@ -0,0 +1,30 @@
+package StringsAndArrays;
+import java.util.*;
+public class RightTrianglePatternLeft {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        if(sc.hasNextInt())
+        {
+            int n = sc.nextInt();
+
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n-i-1; j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int k=0; k<=i; k++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+
+            }
+        }
+        else
+        {
+            System.out.println("Please enter a valid integer");
+        }
+    }
+}
